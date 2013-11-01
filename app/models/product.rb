404debca5bd1a2ee0d55e01be9eeea7e732b865b
Julio Budal <juli0w@hotmail.com>
2013-11-01 01:45:48 -0200

    changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ProductUploader
 
   attr_accessible :category_id, :description, :name, :price,
-                  :image, :image_cache
+                  :image, :image_cache, :color_ids, :size_ids
 
   has_and_belongs_to_many :sizes
   has_and_belongs_to_many :colors
