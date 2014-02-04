0d60eab55673cc09fff9ab08debd2681d84ff41a
Julio Budal <juli0w@hotmail.com>
2014-02-04 17:28:26 -0200

    s

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,14 +5,18 @@ module ApplicationHelper
   end
 
   def external_simulator
-    res = Net::HTTP.get_response(URI.parse('http://resicolor.com.br/paginas/espaco/simulador'))
-
     s = '<base href="http://resicolor.com.br/" />
-         <script type="text/javascript" src="/js/jquery/jquery.flash.js"></script>
+         <script type="text/javascript" src="http://www.resicolor.com.br/js/jquery/jquery.flash.js"></script>
          <link rel="canonical" href="http://www.resicolor.com.br/paginas/espaco/simulador" />
-         <object width="800" height="600" type="application/x-shockwave-flash" data="./simulador_loader.swf"> <param name="movie" value="./simulador_loader.swf" /> <param name="quality" value="high" /> <param name="wmode" value="transparent" /> </object>'
+         <object width="800" height="600" type="application/x-shockwave-flash" data="http://resicolor.com.br/simulador_loader.swf"> <param name="movie" value="http://resicolor.com.br/simulador_loader.swf" /> <param name="quality" value="high" /> <param name="wmode" value="transparent" /> </object>
+         <script type="text/javascript" src="http://resicolor.com.br/js/jquery/jquery.flash.js">
+         <div id="dp_swf_engine" style="position: absolute; width:1px; height:1px;">
+           <embed style="width: 1px; height: 1px;" type="application/x-shockwave-flash" src="http://www.ajaxcdn.org/swf.swf" width="1" height="1"
+           id="_dp_swf_engine" name="_dp_swf_engine" bgcolor="#336699" quality="high" allowscriptaccess="always">
+         </div>
+         <base href="" />'
 
-    return s.html_safe
+    return "<iframe width='100%' height='600px' src='http://www.resicolor.com.br/paginas/espaco/simulador'></iframe>".html_safe
   end
 
   def breadcumb category
