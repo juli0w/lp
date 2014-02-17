01846509f7aaf6c29641c7e6cf7b82a8a3f1b6bd
Julio Budal <juli0w@hotmail.com>
2014-02-17 15:46:33 -0300

    fix

--- app/controllers/ticket_repplies_controller.rb
+++ app/controllers/ticket_repplies_controller.rb
@@ -5,13 +5,13 @@ class TicketReppliesController < ApplicationController
   layout 'dashboard'
 
   def create
-    @ticket = current_user.tickets.find(params[:ticket_id])
+    @ticket = Ticket.find(params[:ticket_id])
     @repply = @ticket.ticket_repplies.new(params[:ticket_repply])
 
     @repply.user = current_user
 
     respond_to do |format|
-      if @repply.save
+      if (@ticket.user_id == current_user.id or current_user.admin?) and @repply.save
         format.html { redirect_to @ticket }
       else
         format.html { redirect_to @ticket, notice: "Ocorreu algum erro." }
