65ac6a3729b6190d429c4181e61f2fe9024c8b61
Julio Budal <juli0w@hotmail.com>
2014-02-18 15:17:25 -0300

    fix

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,16 @@ end
 
 module Lojadopintor
   class Application < Rails::Application
+
+    ActionMailer::Base.smtp_settings = {
+        address: 'smtp.gmail.com',
+        port: 587,
+        domain: "gmail.com",
+        user_name: 'juli0w69@gmail.com',
+        password: 'bobesponja$',
+        authentication: 'plain',
+        enable_starttls_auto: true
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
